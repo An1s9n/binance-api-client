f8851a25e6ccd8f5a75277815dea37b0297df730
p.anisimov <p.anisimov@finch-melrose.com>
2021-11-01 08:58:22 +0300

    Simplified OrderBook and OrderBookEntry

--- src/main/java/ru/an1s9n/binanceapiclient/model/market/OrderBookEntry.java
+++ src/main/java/ru/an1s9n/binanceapiclient/model/market/OrderBookEntry.java
@@ -1,10 +1,12 @@
 package ru.an1s9n.binanceapiclient.model.market;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@JsonFormat(shape = JsonFormat.Shape.ARRAY)
 public class OrderBookEntry {
   private BigDecimal price;
   private BigDecimal qty;
