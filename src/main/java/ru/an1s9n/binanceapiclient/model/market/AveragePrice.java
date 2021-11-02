b04aedd5de6cf539eab15d4ed32d6bea1279649f
p.anisimov <p.anisimov@finch-melrose.com>
2021-11-02 06:46:15 +0300

    Fixed bug in AveragePrice

--- src/main/java/ru/an1s9n/binanceapiclient/model/market/AveragePrice.java
+++ src/main/java/ru/an1s9n/binanceapiclient/model/market/AveragePrice.java
@@ -2,10 +2,10 @@ package ru.an1s9n.binanceapiclient.model.market;
 
 import lombok.Data;
 
-import java.math.BigInteger;
+import java.math.BigDecimal;
 
 @Data
 public class AveragePrice {
   private Integer mins;
-  private BigInteger price;
+  private BigDecimal price;
 }
