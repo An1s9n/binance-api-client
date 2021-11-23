e6ef01ccc950e6279bca4fab681f610f5be347be
An1s9n <An1s9n@yandex.ru>
2021-11-23 09:05:39 +0300

    Added getById method to KlineInterval

--- src/main/java/ru/an1s9n/binanceapiclient/model/market/KlineInterval.java
+++ src/main/java/ru/an1s9n/binanceapiclient/model/market/KlineInterval.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Getter
 public enum KlineInterval {
@@ -25,4 +28,18 @@ public enum KlineInterval {
   @JsonEnumDefaultValue UNKNOWN("unknown");
 
   private final String id;
+
+  private static final Map<String, KlineInterval> id2KlineInterval;
+  static {
+    id2KlineInterval = new HashMap<>();
+    for (KlineInterval i : KlineInterval.values()) {
+      id2KlineInterval.put(i.id, i);
+    }
+  }
+
+  public static KlineInterval getById(String id) {
+    final var klineInterval = id2KlineInterval.get(id);
+    return klineInterval == null ? UNKNOWN : klineInterval;
+  }
+
 }
