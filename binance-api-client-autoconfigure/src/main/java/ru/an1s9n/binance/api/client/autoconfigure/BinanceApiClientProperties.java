be6c70c90300f51a39a231455dc47ef96c0b009d
An1s9n <An1s9n@yandex.ru>
2021-12-20 09:10:19 +0300

    Add JavaDoc to BinanceApiClientProperties

--- binance-api-client-autoconfigure/src/main/java/ru/an1s9n/binance/api/client/autoconfigure/BinanceApiClientProperties.java
+++ binance-api-client-autoconfigure/src/main/java/ru/an1s9n/binance/api/client/autoconfigure/BinanceApiClientProperties.java
@@ -2,5 +2,9 @@ package ru.an1s9n.binance.api.client.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * @param key       Binance api key for accessing endpoints which require it
+ * @param secretKey Binance api secret key for accessing endpoints which require it
+ */
 @ConfigurationProperties("binance.api")
-public record BinanceApiClientProperties (String key, String secretKey) {}
+public record BinanceApiClientProperties(String key, String secretKey) {}
