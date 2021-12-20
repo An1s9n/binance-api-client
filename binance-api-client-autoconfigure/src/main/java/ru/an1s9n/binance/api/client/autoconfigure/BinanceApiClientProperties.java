f8db3238de9699017d2651f58fd47b011b12637a
An1s9n <An1s9n@yandex.ru>
2021-12-20 18:42:31 +0300

    Add additional-spring-configuration-metadata.json

--- binance-api-client-autoconfigure/src/main/java/ru/an1s9n/binance/api/client/autoconfigure/BinanceApiClientProperties.java
+++ binance-api-client-autoconfigure/src/main/java/ru/an1s9n/binance/api/client/autoconfigure/BinanceApiClientProperties.java
@@ -3,8 +3,8 @@ package ru.an1s9n.binance.api.client.autoconfigure;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * @param key       Binance api key for accessing endpoints which require it
- * @param secretKey Binance api secret key for accessing endpoints which require it
+ * @param key       Binance api key for accessing Binance endpoints which require it.
+ * @param secretKey Binance api secret key for accessing Binance endpoints which require it.
  */
 @ConfigurationProperties("binance.api")
 public record BinanceApiClientProperties(String key, String secretKey) {}
